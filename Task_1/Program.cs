db10e238ac9d2c1e7ad5b91a78ee5b3966b6258e
Sergey Zavyalov <ser500vms@gmail.com>
2023-07-07 13:24:34 +0300

    done task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+// Алгоритм: 
+// 1. Сгенирировать число в промежутке от 10 до 99.
+// 2. Разделить число на две цифры.
+// 3. Сравнить и вывести большее.
+
+int number = new Random().Next(10, 100); // Сгенирировать число в промежутке от 10 до 99.
+int firstDigit = number / 10;
+int secondDigit = number % 10; // Разделить число на две цифры.
+int max = firstDigit;
+if (max < secondDigit)
+{
+    max = secondDigit;
+}
+Console.WriteLine($"Максимальная цифра в числе {number} -> {max}"); // Сравнить и вывести большее.
